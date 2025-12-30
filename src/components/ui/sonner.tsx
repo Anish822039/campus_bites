ea85339d58d1059ffe5fcfb1f568d36ebe79de38
Anish822039 <146845429+Anish822039@users.noreply.github.com>
2025-12-31 01:33:18 +0530

    Update sonner.tsx

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,4 +1,6 @@
 import { useTheme } from "next-themes";
+import { Toaster } from "@/components/ui/sonner"
+
 import { Toaster as Sonner, toast } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
